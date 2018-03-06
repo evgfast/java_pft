75c552046e8998ba6382f418cfa6378d92674349
Evgeniy <evgfast@gmail.com>
2018-03-06 23:25:21 +0300

    Finish second lab

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase {
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test_modication", "modification3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
